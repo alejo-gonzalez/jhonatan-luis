30c6ee28f1e983ce0aacb3bd7f421e7580e3fa79
luismg9415 <luismg9415@hotmail.com>
2013-09-12 10:48:49 +0200

    hola

---
+++ jhonatanluis/src/jhonatanluis/Jhonatanluis.java
@@ -0,0 +1,17 @@
+
+package jhonatanluis;
+
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
+public class Jhonatanluis {
+
+    
+    public static void main(String[] args) {
+        int x=2,y=4,s;
+        
+        s=x+y;
+        
+        System.out.println(s);
+        
+    }
+}
